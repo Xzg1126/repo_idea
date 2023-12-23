04d543973fa2251de8b26d4fb6e6f13b2846ef86
Xzg1126 <154665751+Xzg1126@users.noreply.github.com>
2023-12-23 18:46:43 +0800

    Update testMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/xzg/dao/testMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/xzg/dao/testMapper.java
@@ -10,4 +10,10 @@ public interface testMapper {
 
     public List<test> findAlltest();
       public List<test> findAlltest1();
+
+    public void test11();
+      public void test21();
+      public void test31();
+      public void test41();
+      public void test51();
 }
